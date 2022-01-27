acb6806075fcff067bc3f1706e652e8505d59347
Paul Kenny <paul.s.kenny@gmail.com>
2022-01-27 16:24:03 +0000

    Update

--- scripts/ready-deploy.sh
+++ scripts/ready-deploy.sh
@@ -3,10 +3,11 @@
 cd ..
 stack clean
 stack build
+stack exec site clean
 stack exec site build
 
 # Run lychee for link rot https://github.com/lycheeverse/lychee
 echo Running Lychee...
 cd docs
-lychee *.html
+lychee *.html **/*.html **/**/*.html
 
