d5b27ddcccabd9beed371e75b4ee2fc59d41abba
Paul Kenny <paul.s.kenny@gmail.com>
2022-01-29 20:40:36 +0000

    Add faq reference

--- scripts/ready-deploy.bat
+++ scripts/ready-deploy.bat
@@ -2,5 +2,6 @@ cd ..
 stack clean
 stack build
 stack exec site clean
+:: See https://jaspervdj.be/hakyll/tutorials/faq.html#hgetcontents-invalid-argument-or-commitbuffer-invalid-argument
 chcp 65001
 stack exec site build
